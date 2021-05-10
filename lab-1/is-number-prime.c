ca31f539337a3cd9ba12536c6a3f2ffc8684b8e1
chirumer <71926497+chirumer@users.noreply.github.com>
2021-05-10 09:30:37 +0530

    Create is-number-prime.c

---
+++ lab-1/is-number-prime.c
@@ -0,0 +1,25 @@
+// checks if input is prime
+
+#include <stdio.h> // printf(), scanf()
+#include <math.h> // sqrt()
+#include <stdbool.h> // bool
+
+bool is_prime(int input);
+
+int main() {
+
+    printf("Enter a number: ");
+    int input;
+    scanf("%d", &input);
+
+    printf(is_prime(input)? "Prime" : "Not Prime");
+    return 0;
+}
+
+bool is_prime(int input) {
+    if (input < 2 || input % 2 == 0) { return false; }
+    for (int i = 3; i <= sqrt(input); ++i) {
+        if (input % i == 0) { return false; }
+    }
+    return true;
+}
