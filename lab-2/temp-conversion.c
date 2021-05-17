1abb08caeb6c147dd5b69236b942d6ba96939ec9
chirumer <71926497+chirumer@users.noreply.github.com>
2021-05-17 10:16:11 +0530

    Update temp-conversion.c

--- lab-2/temp-conversion.c
+++ lab-2/temp-conversion.c
@@ -3,13 +3,13 @@
 int main() {
 
     printf("Enter temperature value  (in Fahrenheit): ");
-    int fahrenheit;
-    scanf("%d", &fahrenheit);
+    double fahrenheit;
+    scanf("%lf", &fahrenheit);
 
-    int celsius = (5.0/9.0)*(fahrenheit-32);
+    double celsius = (5.0/9.0)*(fahrenheit-32);
 
-    printf("temperature in Fahrenheit: %d", fahrenheit);
-    printf("\ntemperature in Celsius: %d\n", celsius);
+    printf("temperature in Fahrenheit: %lf", fahrenheit);
+    printf("\ntemperature in Celsius: %lf\n", celsius);
 
     return 0;
 }
